f4c80272f9517a3f52d5aa6835fd754411b05863
Bcecil <blakemadness@gmail.com>
2019-12-09 17:55:59 -0800

    renamed to .php and added featured podcast query

--- CS340FinalProject/index.php
+++ CS340FinalProject/index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <?php require_once "connectvars.php"; ?>
     <script
       src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
       src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js">
@@ -42,10 +43,33 @@
 
     <div class="container">
       <h1>Featured Podcasts:</h1>
-      <h3>Top 3:</h3>
-
-
-
+      <?php  $sql = "SELECT Pname, Gtype FROM Podcast ORDER BY rating DESC LIMIT 3";
+      if($result = mysqli_query($link, $sql)){
+          if(mysqli_num_rows($result) > 0){
+            echo "<table class='table table-bordered table-striped'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>Podcast Name</th>";
+            echo "<th>Genre</th>";
+            echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>" . $row['Pname'] . "</td>";
+                    echo "<td>" . $row['Gtype'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";
+                echo "</table>";
+                mysqli_free_result($result);
+            } else {
+                echo "<p class='lead'><em>No records were found.</em></p>";
+            }
+        } else {
+          echo "ERROR" . mysqli_error($link);
+        }
+        ?>
     </div>
 
   </body>
