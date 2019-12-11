99a9e7486cea9fe39e97b13a5fe250f5e225d9aa
Bcecil <blakemadness@gmail.com>
2019-12-10 17:38:01 -0800

    initial commit

---
+++ CS340FinalProject/success.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!-- Add Part Info to Table Part -->
+<?php
+		$currentpage="Succes";
+    require_once "connectvars.php";
+?>
+<html>
+	<head>
+		<title>Success</title>
+		<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	  <script
+	    src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js">
+	  </script>
+    <style type="text/css">
+      .err{
+        color: red;
+      }
+    </style>
+
+	<!-- Navbar: -->
+	  <nav class="navbar navbar-inverse">
+	    <div class="container-fluid">
+	      <div class="navbar-header">
+	        <a class="navbar-brand" href="index.html">PodWiki</a>
+	      </div>
+	      <ul class="nav navbar-nav">
+	        <li><a href="index.php">Home</a></li>
+	        <li><a href="browse.php">Browse</a></li>
+	        <li><a href="rating.php">Ratings</a></li>
+	        <li><a href="add.php">Insert</a></li>
+	        <li class="active"><a href="update.php">Update</a></li>
+	      </ul>
+	    </div>
+	  </nav>
+	<!-- End Navbar -->
+
+	</head>
+
+
+
+
+<body>
+<h1 style="text-align: center"> Update Succesful <h1>
+</body>
+</html>
+
+
